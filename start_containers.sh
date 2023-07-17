fec199bc4d724649522a59de00b305a8b4660b08
Will Breiler <wbreiler@Wills-MacBook-Air.local>
2023-07-17 16:43:21 -0500

    Added Portainer docker-compose and added Portainer to start script

--- start_containers.sh
+++ start_containers.sh
@@ -4,6 +4,6 @@ gitea="-f gitea.yml"
 minecraft="-f minecraft.yml"
 speedtest="-f speedtest.yml"
 tubearchivist="-f tubearchivist.yml"
+portainer="-f portainer.yml"
 
-
-docker-compose $gitea $minecraft $speedtest $tubearchivist up -d
+docker-compose $portainer $gitea $minecraft $speedtest $tubearchivist up -d
