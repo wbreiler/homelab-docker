72ab05bf457542822c35790129339af0ace06adf
Will Breiler <wbreiler@Wills-MacBook-Air.local>
2023-07-21 11:25:52 -0500

    Added Pi-hole

--- start_containers.sh
+++ start_containers.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# I am 100% certain there is a more effecient way to do this, but this is what *I* have come up with in the short term
+
 gitea="-f gitea.yml"
 minecraft="-f minecraft.yml"
 speedtest="-f speedtest.yml"
 tubearchivist="-f tubearchivist.yml"
 portainer="-f portainer.yml"
+pihole="-f pihole.yml"
 
-docker-compose $portainer $gitea $minecraft $speedtest $tubearchivist up -d
+docker-compose $portainer $gitea $pihole $minecraft $speedtest $tubearchivist up -d
