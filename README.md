500ee161e03335c8e96a99696b3a30377b9fa700
Will Breiler <will.breiler@gmail.com>
2023-10-02 18:32:56 -0500

    Copied from http://git.wbreiler.com/

---
+++ README.md
@@ -0,0 +1,37 @@
+# Containerized homelab
+
+This project provides a collection of Docker-compose files and configurations for running various services in a home lab environment.
+
+## Prerequisites
+
+- Root access
+- Time
+- Patience
+
+## Installation
+
+1. Clone this repository:
+```shell
+git clone https://git.wbreiler.com/wbreiler/homelab-docker.git
+```
+2. Navigate to the project directory:
+```shell
+cd homelab-docker
+```
+3. Customize the files in [/containers](containers/)
+
+4. Run `setup.sh`
+
+## Services
+
+- `dashboard.yml` is a dashboard for all of the services, so you don't have to remember all the IPs and ports
+- `gitea.yml` is a locally hosted Git instance, similar to GitHub or GitLab
+- Further information can be found [here](containers/README.MD)
+
+## Contributing
+
+Contributions are welcome! Please fork this repository and submit a pull request with your changes.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
--- setup.sh
+++ setup.sh
@@ -1,13 +1,61 @@
 #!/bin/bash
 
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+echo "Welcome to my homelab installation and bootstrapping script"
+
 # Install Cockpit first
-chmod +x scripts/install_cockpit.sh
-./scripts/install_cockpit.sh
+# Check if Cockpit is already installed
+check_cockpit_installed() {
+    if command -v cockpit &>/dev/null; then
+        echo "Cockpit is already installed on your system."
+        return 0
+    else
+        return 1
+    fi
+}
+
+install_cockpit() {
+  echo "Installing Cockpit..."
+  sudo apt update
+  sudo apt install -y cockpit
+
+  # Enable and start the Cockpit service
+  sudo systemctl enable --now cockpit.socket
+
+  # Add the current user to the 'cockpit' group to allow access without password
+  sudo usermod -aG cockpit $(whoami)
+
+  echo "Cockpit has been successfully installed!"
+  echo "You can access Cockpit by opening a web browser and navigating to: https://localhost:9090/"
+}
+
+echo "Welcome to the Cockpit portion of this installation script"
+echo "This will help you install the latest version of Cockpit."
+echo "Do you want to continue? (y/n)"
+
+read -r response
+
+if [[ "$response" =~ ^[Yy]$ ]]; then
+    if check_cockpit_installed; then
+        echo "Do you still want to proceed with the installation? (y/n)"
+        read -r continue_response
+        if ! [[ "$continue_response" =~ ^[Yy]$ ]]; then
+            echo "Exiting script."
+            exit 0
+        fi
+    fi
+
+    install_cockpit
+else
+    echo "Installation cancelled."
+    exit 0
+fi
 
 
 # Install Netdata
-# chmod +x scripts/install_netdata.sh
-# ./scripts/install_netdata.sh
 sh /home/ubuntu/scripts/netdata-kickstart.sh --disable-telemetry
 
 #Function to check if Docker is already installed
@@ -50,9 +98,9 @@ install_docker() {
 
 # Main script
 
-echo "Welcome to the Docker installation script for Ubuntu."
-echo "This script will help you install the latest version of Docker."
-echo "Do you want to continue with the installation? (y/n)"
+echo "Welcome to the Docker installation portion of this script."
+echo "This will help you install the latest version of Docker."
+echo "Do you want to continue? (y/n)"
 
 read -r response
 
@@ -73,9 +121,82 @@ else
 fi
 
 # Install Docker Compose
-chmod +x scripts/install_compose.sh
-./scripts/install_compose.sh
+
+check_compose_installed() {
+    if command -v docker-compose &>/dev/null; then
+        echo "Docker Compose is already installed on your system."
+        echo "Current vuersion:"
+        docker-compose --version
+        return 0
+    else
+        return 1
+    fi
+}
+
+install_compose() {
+    echo "Installing Docker Compose..."
+    # Download the latest version of Docker Compose
+    curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+    # Apply executable rights to the binary
+    chmod +x /usr/local/bin/docker-compose
+
+    # Create a symlink to make docker-compose accesible from the terminal
+    ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+    echo "Docker Compose has been successfully installed"
+    echo "Version:"
+    docker-compose --version
+}
+
+echo "Welcome to the Docker Compose portion of this script."
+echo "This will help you install the latest version of Docker Compose."
+echo "Do you want to continue? (y/n)"
+
+if [[ "$response" =~ ^[Yy]$ ]]; then
+    if check_compose_installed; then
+        echo "Do you still want to proceed with the installation? (y/n)"
+        read -r continue_response
+        if ! [[ "$continue_response" =~ ^[Yy]$ ]]; then
+            echo "Exiting script."
+            exit 0
+        fi
+    fi
+
+    install_compose
+else
+    echo "Installation cancelled."
+    exit 0
+fi
 
 # Start containers via docker-compose
-chmod +x scripts/start_containers.sh
-sudo ./scripts/start_containers.sh
+start_compose() {
+  echo "Starting Docker Compose for file: $1"
+  docker-compose -f "$1" up -d
+}
+
+# Check if any Docker Compose files are present in the containers directory
+compose_files=$(ls containers/*.yml 2>/dev/null)
+if [ -n "$compose_files" ]; then
+  echo "Found Docker Compose files in the containers directory."
+
+  # Prompt the user for confirmation before starting
+  read -p "Do you wish to start all Docker Compose files in this directory? (y/n): " choice
+  case "$choice" in
+    y|Y )
+      for file in $compose_files; do
+        start_compose "$file"
+      done
+      ;;
+    n|N )
+      echo "Starting cancelled. Exiting..."
+      exit 0
+      ;;
+    * )
+      echo "Invalid choice. Please choose 'y' for yes or 'n' for no."
+      exit 1
+      ;;
+  esac
+else
+  echo "No Docker Compose files found in the containers directory."
+fi
